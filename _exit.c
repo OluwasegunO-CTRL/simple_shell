76c0fc087d5148ac08bdfe851992e3ff9a35f13c
segun <70609367+OluwasegunO-CTRL@users.noreply.github.com>
2022-08-11 02:41:55 +0100

    add exit file

--- _exit.c
+++ _exit.c
@@ -1,16 +1,13 @@
 #include "main.h"
-
 /**
  * my_exit - exits the shell
  *
  * @args: args passed
  * Return: Nothing
  */
-
 int my_exit(char **args)
 {
-		free(args);
-
-		/*exit(98)*/
-		exit(0);
+	free(args);
+	/*exit(98)*/
+	exit(0);
 }
